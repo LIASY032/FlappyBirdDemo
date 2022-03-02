7f1fafd4eaaa491213a398077fafa3ecff80a6ae
LIASY032 <66517361+LIASY032@users.noreply.github.com>
2022-03-02 20:00:14 +1030

    assets added

--- core/src/com/fan/game/FlappyDemo.java
+++ core/src/com/fan/game/FlappyDemo.java
@@ -1,30 +1,41 @@
 package com.fan.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.fan.game.State.GameStateManager;
+import com.fan.game.State.MenuState;
 
 public class FlappyDemo extends ApplicationAdapter {
 	public static final int WIDTH = 480;
 	public static final int HEIGHT = 800;
 	public static final String TITLE = "Flappy Bird";
 
-	SpriteBatch batch;
+	private GameStateManager gsm;
+	private SpriteBatch batch;
 	Texture img;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		gsm = new GameStateManager();
+//		img = new Texture("badlogic.jpg");
+
+		gsm.push(new MenuState(gsm));
+
 	}
 
 	@Override
 	public void render () {
+//		batch.begin();
+//		batch.draw(img, 0, 0);
+//		batch.end();
+
 		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+		gsm.update(Gdx.graphics.getDeltaTime());
+		gsm.render(batch);
 	}
 	
 	@Override
---
+++ core/src/com/fan/game/State/MenuState.java
@@ -0,0 +1,29 @@
+package com.fan.game.State;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class MenuState extends State{
+    public MenuState(GameStateManager gam) {
+        super(gam);
+    }
+
+    @Override
+    protected void handleInput() {
+
+    }
+
+    @Override
+    protected void update(float dt) {
+
+    }
+
+    @Override
+    public void render(SpriteBatch sb) {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
