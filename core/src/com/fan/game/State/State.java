40989b76f6ab872ddf330aaf432acd4ec5c2f8b8
LIASY032 <66517361+LIASY032@users.noreply.github.com>
2022-03-02 17:00:40 +1030

    first commit

---
+++ core/src/com/fan/game/State/State.java
@@ -0,0 +1,37 @@
+//package com.fan.game.State;
+//
+//import com.badlogic.gdx.graphics.OrthographicCamera;
+//import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+//import com.badlogic.gdx.math.Vector3;
+//
+//public abstract class State {
+//
+//    protected OrthographicCamera cam;
+//
+//protected GameStateManager gam;
+//    protected Vector3 mouse;
+//
+//
+//
+//
+//    protected  State(GameStateManager gam){
+//
+//        this.gam = gam;
+//cam = new OrthographicCamera();
+//
+//mouse = new Vector3();
+//
+//
+//    }
+//
+//
+//    protected abstract void handleInput();
+//
+//
+//    protected  abstract void update(float dt);
+//
+//    public abstract void render(SpriteBatch sb);
+//    public abstract void dispose();
+//
+//
+//}
