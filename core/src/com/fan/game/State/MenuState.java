a603bfa099480809db6426eb6fbef23848d0a12a
LIASY032 <66517361+LIASY032@users.noreply.github.com>
2022-03-03 13:54:34 +1030

    menu added

--- core/src/com/fan/game/State/MenuState.java
+++ core/src/com/fan/game/State/MenuState.java
@@ -1,29 +1,45 @@
 package com.fan.game.State;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.fan.game.FlappyDemo;
 
 public class MenuState extends State{
-    public MenuState(GameStateManager gam) {
-        super(gam);
+    private Texture background;
+    private Texture playBtn;
+    public MenuState(GameStateManager gsm) {
+        super(gsm);
+        cam.setToOrtho(false, FlappyDemo.WIDTH / 2, FlappyDemo.HEIGHT / 2);
+        background = new Texture("bg.png");
+        playBtn = new Texture("playbtn.png");
     }
 
     @Override
-    protected void handleInput() {
-
+    public void handleInput() {
+//        if(Gdx.input.justTouched()){
+//            gsm.set(new PlayState(gsm));
+//        }
     }
 
     @Override
-    protected void update(float dt) {
-
+    public void update(float dt) {
+        handleInput();
     }
 
     @Override
     public void render(SpriteBatch sb) {
-
+        sb.setProjectionMatrix(cam.combined);
+        sb.begin();
+        sb.draw(background, 0,0);
+        sb.draw(playBtn, cam.position.x - playBtn.getWidth() / 2, cam.position.y);
+        sb.end();
     }
 
     @Override
     public void dispose() {
-
+        background.dispose();
+        playBtn.dispose();
+        System.out.println("Menu State Disposed");
     }
 }
--- core/src/com/fan/game/State/State.java
+++ core/src/com/fan/game/State/State.java
@@ -1,11 +1,11 @@
 package com.fan.game.State;
 
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector3;
 
 public abstract class State {
-
     protected OrthographicCamera cam;
 
 protected GameStateManager gam;
