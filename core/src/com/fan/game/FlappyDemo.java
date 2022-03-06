04bf79d6fe79244b3fe6c60f941216e7f4d3053a
LIASY032 <66517361+LIASY032@users.noreply.github.com>
2022-03-06 17:11:50 +1030

    Update FlappyDemo.java

--- core/src/com/fan/game/FlappyDemo.java
+++ core/src/com/fan/game/FlappyDemo.java
@@ -2,45 +2,47 @@ package com.fan.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.graphics.GL20;
 import com.fan.game.states.GameStateManager;
 import com.fan.game.states.MenuState;
 
 public class FlappyDemo extends ApplicationAdapter {
 	public static final int WIDTH = 480;
 	public static final int HEIGHT = 800;
-	public static final String TITLE = "Flappy Bird";
 
+	public static final String TITLE = "Flappy Bird";
 	private GameStateManager gsm;
 	private SpriteBatch batch;
-	Texture img;
-	
+
+	private Music music;
+
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		gsm = new GameStateManager();
-		img = new Texture("badlogic.jpg");
-
+		music = Gdx.audio.newMusic(Gdx.files.internal("music.mp3"));
+		music.setLooping(true);
+		music.setVolume(0.1f);
+		music.play();
+		Gdx.gl.glClearColor(1, 0, 0, 1);
 		gsm.push(new MenuState(gsm));
-
 	}
 
 	@Override
 	public void render () {
-//		batch.begin();
-//		batch.draw(img, 0, 0);
-//		batch.end();
-
-		ScreenUtils.clear(1, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		gsm.update(Gdx.graphics.getDeltaTime());
 		gsm.render(batch);
 	}
-	
+
 	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
+	public void dispose() {
+		super.dispose();
+		music.dispose();
 	}
 }
